45dea515a2523ceef9459767209c0161e48cfb2e
Biwei Xie <biweixie@BiweideMacBook-Pro.local>
2021-12-05 18:14:52 -0800

    first commit

---
+++ aws-cognito/src/components/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function Hero() {
+  return (
+    <section className="hero is-medium is-light is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Primary title</h1>
+        </div>
+      </div>
+    </section>
+  )
+}
